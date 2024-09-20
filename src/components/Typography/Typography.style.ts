8377cc0751df02811beb0943785c300a5446ae23
bliss <kyo8747@naver.com>
2024-09-20 13:03:43 +0900

    [FEAT] (#11) Typography 제작

--- src/components/Typography/Typography.style.ts
+++ src/components/Typography/Typography.style.ts
@@ -1,3 +1,37 @@
 import styled from "@emotion/styled";
+import { Property } from "csstype";
 
-export const TypographyContainer = styled.div``;
+export type TTypographyStyle = "center" | "left" | "right";
+
+export interface ITypographyStyle {
+  align?: TTypographyStyle;
+  color?: string;
+  fontSize?: Property.FontSize;
+  fontWeight?: Property.FontWeight;
+  fontFamily?: string;
+  lineHeight?: Property.LineHeight;
+  textDecoration?: Property.TextDecoration;
+  textDecorationColor?: string;
+  width?: string;
+}
+
+export const TypographyContainer = styled.span<ITypographyStyle>`
+  display: inline-block;
+
+  text-align: ${({ align }) => align};
+  color: ${({ color }) => color};
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  font-family: ${({ fontFamily }) => fontFamily};
+  line-height: ${({ lineHeight }) => lineHeight};
+  width:  ${({ width }) => width};
+
+  ${({ textDecoration, textDecorationColor }) =>
+    textDecoration &&
+    `
+      text-decoration: ${textDecoration};
+      text-decoration-thickness: 2px;
+      text-underline-position: under;
+      text-decoration-color: ${textDecorationColor};
+    `}
+`;
