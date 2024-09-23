7f02513f238b51c9c25f4f3be8a113d7a2f35e11
summer0573 <s2113@e-mirim.hs.kr>
2024-09-23 15:37:54 +0900

    [FEAT] (#12) 테이블 컴포넌트 제작 작업

--- src/components/Table/Table.stories.ts
+++ src/components/Table/Table.stories.ts
@@ -1,5 +1,5 @@
 import { StoryObj } from "@storybook/react";
-import Table from "./index";
+import { Table } from "./index";
 
 const meta = {
   title: "Table",
@@ -15,6 +15,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
-    children: "Default Table",
+    caption: 'caption',
+    children: 'Table'
   },
 };
--- src/components/Table/Table.style.ts
+++ src/components/Table/Table.style.ts
@@ -1,3 +1,20 @@
 import styled from "@emotion/styled";
 
-export const TableContainer = styled.div``;
+export type TTdAlign = "left" | "center" | "right";
+
+export interface ITableContainer {
+  width?: string;
+  height?: string;
+}
+
+export interface ITDContainer {
+  align?: TTdAlign;
+}
+
+export const TableContainer = styled.table<ITableContainer>``;
+export const THeadContainer = styled.thead``;
+export const TBodyContainer = styled.tbody``;
+export const TFootContainer = styled.tfoot``;
+export const TRContainer = styled.tr``;
+export const THContainer = styled.th``;
+export const TDContainer = styled.td<ITDContainer>``;
