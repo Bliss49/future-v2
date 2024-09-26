68078ffc5db4e94f0c9cdb1fc9fbccd9cac67110
summer0573 <s2113@e-mirim.hs.kr>
2024-09-26 11:18:44 +0900

    [FEAT] (#19)  버튼 컴포넌트 수정 롤백

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -1,6 +1,5 @@
 import { StoryObj } from "@storybook/react";
 import Button from "./index";
-import { EButtonShape, EButtonStyle } from "./Button.style";
 
 const meta = {
   title: "Button",
@@ -10,58 +9,11 @@ const meta = {
   },
   tags: ["autodocs"],
 };
-
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
-    ariaLabel: "button",
     children: "Default Button",
-    color: EButtonStyle.NORMAL,
-  },
-};
-
-export const Round: Story = {
-  args: {
-    ariaLabel: "button",
-    children: "Default Button",
-    color: EButtonStyle.DEFAULT,
-    shape: EButtonShape.ROUND,
-  },
-};
-
-export const Shadow: Story = {
-  args: {
-    ariaLabel: "button",
-    children: "Default Button",
-    color: EButtonStyle.SECONDARY,
-    shape: EButtonShape.SHADOW,
-  },
-};
-
-export const Yellow: Story = {
-  args: {
-    ariaLabel: "button",
-    children: "Default Button",
-    color: EButtonStyle.YELLOW,
-  },
-};
-
-export const Brown: Story = {
-  args: {
-    ariaLabel: "button",
-    children: "Default Button",
-    color: EButtonStyle.BROWN,
-    shape: EButtonShape.ROUND,
-  },
-};
-
-export const Blue: Story = {
-  args: {
-    ariaLabel: "button",
-    children: "Default Button",
-    color: EButtonStyle.BLUE,
-    shape: EButtonShape.SHADOW,
   },
 };
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,50 +1,7 @@
-import {
-  ButtonContainer,
-  EButtonShape,
-  EButtonStyle,
-  IButtonStyled,
-} from "./Button.style";
+import { ButtonContainer } from "./Button.style";
 
-interface IButtonProps extends IButtonStyled {
-  ariaLabel: string;
-  title?: string;
-  disabled?: boolean;
-  children: React.ReactNode;
-  style?: React.CSSProperties;
-  onClick?: () => void;
-}
-
-const Button = ({
-  ariaLabel,
-  title = "",
-  disabled = false,
-  color = EButtonStyle.DEFAULT,
-  shape = EButtonShape.DEFAULT,
-  size = "medium",
-  children,
-  style,
-  onClick,
-}: IButtonProps) => {
-  const handleClick = () => {
-    if (!disabled && onClick) {
-      onClick();
-    }
-  };
-  return (
-    <ButtonContainer
-      type="button"
-      aria-label={ariaLabel}
-      title={title}
-      color={color}
-      disabled={disabled}
-      shape={shape}
-      size={size}
-      style={style}
-      onClick={handleClick}
-    >
-      {children}
-    </ButtonContainer>
-  );
+const Button = () => {
+  return <ButtonContainer>Button</ButtonContainer>;
 };
 
 export default Button;
