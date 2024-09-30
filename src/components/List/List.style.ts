c571e89f98d1b48e28d9e9b28d919e51f2294eee
summer0573 <s2113@e-mirim.hs.kr>
2024-09-30 17:01:23 +0900

    Revert "[FEAT] (#21) checkBox 컴포넌트 제작"
    This reverts commit 72261ef7874bc3c44c3d181293fcae1ebdb03922.

--- src/components/List/List.style.ts
+++ src/components/List/List.style.ts
@@ -1,18 +1,3 @@
 import styled from "@emotion/styled";
 
-type TDirection = "horizontal" | "vertical";
-
-export interface IListStyle {
-  align?: TDirection;
-  gap?: string;
-}
-
-export const ListContainer = styled.ul<IListStyle>`
-  ${({ align }) => align === "horizontal" && `display:flex;`}
-`;
-export const ListItem = styled.li<IListStyle>`
-  :not(:first-of-type) {
-    ${({ align, gap = 10 }) =>
-      align === "horizontal" ? `margin-left: ${gap};` : `margin-top: ${gap};`}
-  }
-`;
+export const ListContainer = styled.div``;
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,40 +1,7 @@
-import { IListStyle, ListContainer, ListItem } from "./List.style";
+import { ListContainer } from "./List.style";
 
-interface IListRowProps<T> {
-  index?: number;
-  value?: T;
-  onClick?: (state: T) => void;
-}
-
-export interface IList<T> extends IListStyle, IListRowProps<T> {
-  data: T[];
-  children:
-    | React.ReactNode
-    | ((
-        props: IListRowProps<T>
-      ) => React.ReactElement<any, string | React.JSXElementConstructor<any>>);
-}
-
-export function List<T>({
-  align = "vertical",
-  gap = "24px",
-  data,
-  children,
-}: IList<T>) {
-  return (
-    <ListContainer align={align}>
-      {typeof children === "function"
-        ? data?.map((val, idx) => (
-            <ListItem key={`list-row-${idx}`} align={align} gap={gap}>
-              {children({
-                index: idx + 1,
-                value: val,
-              })}
-            </ListItem>
-          ))
-        : children}
-    </ListContainer>
-  );
-}
+const List = () => {
+  return <ListContainer>List</ListContainer>;
+};
 
 export default List;
